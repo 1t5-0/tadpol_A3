502619cc98e9df1171044d17ce6c471d374d29a5
1t5-0 <tsalvad2003@gmail.com>
2024-03-19 16:07:19 -0400

    hi

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,54 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+     import '../style.css';
+     let tadpol = '';
+     let foamnest = [];
+
+     function addToArray() {
+          if (tadpol == '') {
+               return;
+          }
+          foamnest = [...foamnest, {
+               value: tadpol,
+               done: false
+          }];
+          console.log(foamnest);
+          tadpol = ''
+     }
+     function removeThis(index) {
+          foamnest.splice(index, 1);
+          foamnest = foamnest;
+     }
+</script>
+
+<h1>Hop To It!</h1>
+
+<form on:submit|preventDefault={addToArray}>
+     <input type="text" bind:value={tadpol}>
+     <button type="submit">&#43;</button>
+</form>
+
+<ul>
+     {#each foamnest as item, index}
+          <li>
+               <input type="checkbox" bind:checked={item.done}>
+               <span class:done={item.done}>{item.value}</span>
+               <span on:click={() => removeThis(index)} class="remove" tabindex="0">&#45</span>
+          </li>
+     {/each}
+</ul>
+
+<style>
+     ul {
+          list-style: none;
+     }
+     li {
+          font-size: 1.3rem;
+     }
+     .done {
+          color: rgb(165, 165, 165);
+          text-decoration: line-through;
+     }
+     .remove {
+          color: red;
+     }
+</style>
